7ff75e8a6ceb5bae77d870a8e04aacb53cc46c81
Arch Mux <archmux@stemux.com>
2022-07-22 10:58:28 -0500

    Function/Website/function.el: Inception
    Define `archmux/skeleton-html' for future automation purposes

---
+++ Function/Website/function.el
@@ -0,0 +1,19 @@
+(define-skeleton archmux/skeleton-html
+  "Generic HTML skeleton."
+  str
+  "<DOCTYPE html>\n"
+  "<html lang='en-us'>\n"
+  "\t<head>\n"
+  "\t\t<meta charset='utf-8'>\n"
+  "\t\t<meta name='author' content='Arch Mux'>\n"
+  "\t\t<link rel='stylesheet' href='/style.css'>\n"
+  "\t\t<title>Arch Mux's Webpage</title>\n"
+  "<base href='.'>\n"
+  "\t</head>\n\n"
+  "\t<body>\n"
+  "\t</body>\n"
+  "<hr/>\n"
+  "This website is inclusively licensed under the <a href='https://viralpubliclicense.org/'>Viral Public License</a>.\n"
+  "<html>\n"
+)
+
