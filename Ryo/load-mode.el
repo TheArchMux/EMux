b1a0283022dd4c534721d5b8c294eefe925ecdbd
Arch Mux <archmux@stemux.com>
2022-07-22 13:36:51 -0500

    Ryo/load-mode.el: Reformat
    	Capitalize comment, load `Gnus' bindings for `ryo-modal-mode',
    	and order sections alphabetically

--- Ryo/load-mode.el
+++ Ryo/load-mode.el
@@ -1,8 +1,16 @@
-;; prefix
-(load-file (concat user-emacs-directory "Ryo/Mode/prefix.el"))
+;; Dired mode
 
-;; dired mode
 (load-file (concat user-emacs-directory "Ryo/Mode/dired.el"))
 
 ;; Info mode
+
 (load-file (concat user-emacs-directory "Ryo/Mode/info.el"))
+
+;; Gnus mode
+
+(load-file (concat user-emacs-directory "Ryo/Mode/Gnus/load.el"))
+
+;; Prefix
+
+(load-file (concat user-emacs-directory "Ryo/Mode/prefix.el"))
+
