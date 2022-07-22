d4823ef3a665c85e60159548135ef490a6e9dba0
Arch Mux <archmux@stemux.com>
2022-07-22 13:30:14 -0500

    Init/theme.el: Change default theme to `doneburn'.

--- Init/theme.el
+++ Init/theme.el
@@ -1,8 +1,9 @@
 ;; (require 'stimmung-themes)
+(load-theme 'doneburn t)
 ;; (load-theme 'less t)
 ;; (load-theme 'mindre t)
 ;; (load-theme 'stimmung-themes-light t)
-(load-theme 'writerish t)
+;; (load-theme 'writerish t)
 ;; (stimmung-themes-load-light)
 
 (nano-modeline-mode)
