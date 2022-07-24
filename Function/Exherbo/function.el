8e72da5304ad9d1712bc337cc293846616ac17b6
Arch Mux <archmux@stemux.com>
2022-07-23 20:33:42 -0500

    Function/Exherbo/function.el: Wrap body of `archmux/exherbo-cd' in `progn'

--- Function/Exherbo/function.el
+++ Function/Exherbo/function.el
@@ -1,15 +1,23 @@
 (defun archmux/exherbo-cd ()
   "Find exheres."
-  (interactive
-   (let* (
-	  (exherbo_directory	"~/Internet/Git/Exherbo/Remote/")
-	  (exheres_user_input	(read-from-minibuffer "Exheres to locate: "))
-	  (find-pattern		(concat "-name " exheres_user_input "*"))
-	  (find-ls-option       '("-type f -ls" . ""))
-	  )
-     (find-dired exherbo_directory find-pattern)
-     )
-   )
+  (interactive)
+  (let* (
+	 (exherbo_directory	"~/Internet/Git/Exherbo/Remote/")
+	 (exheres_user_input	(read-from-minibuffer "Exheres to locate: "))
+	 (find-pattern		(concat "-name " exheres_user_input "*"))
+	 (find-ls-option       '("-ls" . ""))
+	 (package_directory	"~/Internet/Compressed/Package")
+         (buffer-name (format "*Find: %s in %s*" find-pattern exherbo_directory))
+	 )
+    (progn
+      (split-window-below)
+      (other-window 1)
+      (find-dired exherbo_directory find-pattern)
+      (rename-uniquely)
+      (other-window 1)
+      (find-dired package_directory find-pattern)
+      )
+    )
   )
 
 (defun archmux/exherbo-with ()
