e1ab3a0ccb8ffce05d0fcdb01306690dc1a2eb2d
Arch Mux <archmux@stemux.com>
2022-07-25 13:41:43 -0500

    early-init.el: Load `load-relative.elc'
    Will be used through configurations reducing unnecessary complications arising from absolute paths

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;; Package: load-relative
+;;	Eases loading of decentralised files
+;;	Load first allowing it to be used throughout Emacs configuration
+;;	Link: https://github.com/rocky/emacs-load-relative
+
+(load-file "~/Internet/Git/Emacs/Package/Utility/emacs-load-relative/load-relative.elc")
+
