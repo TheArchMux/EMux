4a7a76dec0e9080f0d930ca87f2a04ba1d2e992f
Edward Bates <gitmux@stemux.com>
2022-08-17 10:58:30 -0500

    Configuration/Miscellaneous/Key-Binding/key-binding.el: Rebind `hippie-expand'
    'M-/' -> '<backtab>'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Miscellaneous/Key-Binding/key-binding.el
+++ Configuration/Miscellaneous/Key-Binding/key-binding.el
@@ -1,7 +1,7 @@
 (global-set-key (kbd "C-c f c") 'archmux/open-collection-file)
 (global-set-key (kbd "C-c f p") 'find-file-at-point)
 (global-set-key (kbd "C-c a n") 'gnus-other-frame)
-(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "<backtab>") 'hippie-expand)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 (global-set-key (kbd "C-c o m") 'man)
 (global-set-key (kbd "C-c o w") 'woman)
