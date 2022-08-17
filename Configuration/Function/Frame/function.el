b9ff44d576a5af2b606396c440ca4d642c5a1255
Edward Bates <gitmux@stemux.com>
2022-08-17 11:32:02 -0500

    Configuration/Function/Frame/function.el: Revise `archmux/magit-status-other-frame'
    Verify whether current file is located within a git directory with `magit-git-dir'.
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Function/Frame/function.el
+++ Configuration/Function/Frame/function.el
@@ -7,8 +7,11 @@
 (defun archmux/magit-status-other-frame ()
   "Call `magit-status' in another frame."
   (interactive)
-  (select-frame (make-frame-command))
-  (magit-status)
-  (delete-other-windows)
-  (local-set-key (kbd "q") 'archmux/kill-buffer-and-frame))
+  (when (magit-git-dir)
+      (progn
+	(select-frame (make-frame-command))
+	(magit-status)
+	(delete-other-windows)
+	(local-set-key (kbd "q") 'archmux/kill-buffer-and-frame))))
+
 
