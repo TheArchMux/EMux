bcb3d5899e8e8a82114d9d9695c086c0acf4539b
Edward Bates <gitmux@stemux.com>
2022-08-16 19:10:09 -0500

    Configuration/Function/Frame/function.el: Introduce `archmux/kill-buffer-and-frame'
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Function/Frame/function.el
+++ Configuration/Function/Frame/function.el
@@ -1,3 +1,9 @@
+(defun archmux/kill-buffer-and-frame ()
+  "Kill current buffer and totality of frame."
+  (interactive)
+  (kill-buffer (current-buffer))
+  (kill-frame (frame-selected-window)))
+
 (defun archmux/magit-status-other-frame ()
   "Call `magit-status' in another frame."
   (interactive)
