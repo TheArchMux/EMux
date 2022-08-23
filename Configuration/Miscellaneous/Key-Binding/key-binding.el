a48f0e3f0846f31ebdb61a09826205a806deb46e
Edward Bates <gitmux@stemux.com>
2022-08-23 08:00:18 -0500

    Configuration/Miscellaneous/Key-Binding/key-binding.el: Bind
    Bind key for `eshell' and `tmr'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Miscellaneous/Key-Binding/key-binding.el
+++ Configuration/Miscellaneous/Key-Binding/key-binding.el
@@ -3,6 +3,8 @@
 (global-set-key (kbd "C-c a n") 'gnus-other-frame)
 (global-set-key (kbd "<backtab>") 'hippie-expand)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
+(global-set-key (kbd "C-c o e") 'eshell)
+(global-set-key (kbd "C-c o t") 'tmr)
 (global-set-key (kbd "C-c o m") 'man)
 (global-set-key (kbd "C-c o w") 'woman)
 (global-set-key (kbd "C-'") 'execute-extended-command)
