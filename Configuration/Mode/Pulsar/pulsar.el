9c1c5ff873e4ea70fcc29aed41955b0318dc9281
Edward Bates <gitmux@stemux.com>
2022-08-24 17:23:25 -0500

    Configuration/Mode/Pulsar/pulsar.el: Inception
    Enable `pulsar-global-mode' and define a number of 'pulsar-*' variables.
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ Configuration/Mode/Pulsar/pulsar.el
@@ -0,0 +1,9 @@
+(load-file "~/Internet/Git/Emacs/pulsar/pulsar.el")
+
+(setq pulsar-pulse t)
+(setq pulsar-delay 0.05)
+(setq pulsar-iterations 20)
+(setq pulsar-face 'pulsar-red)
+(setq pulsar-highlight-face 'pulsar-yellow)
+
+(pulsar-global-mode 1)
