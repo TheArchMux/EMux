20229454bf8d10c230d78ff9c2f3807680f50007
Edward Bates <gitmux@stemux.com>
2022-08-24 17:35:04 -0500

    Configuration/Mode/Gnus/gnus.el: Delete unnecessary portion
    Mail is read with `MH-E' now.
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Gnus/gnus.el
+++ Configuration/Mode/Gnus/gnus.el
@@ -2,23 +2,7 @@
 
 (setq gnus-select-method '(nntp "localhost"))
 
-(add-to-list 'gnus-secondary-select-methods
-             '(nnimap "Archmux-local"
-                      (nnimap-address "localhost")
-                      (nnimap-stream shell)
-		      )
-	     )
-
-(setq gnus-message-archive-method
-      '(nnfolder "archive"
-                 (nnfolder-directory   "~/Media/Document/Mail/Archive/")
-                 (nnfolder-active-file "~/Media/Document/Mail/Archive/active")
-                 (nnfolder-get-new-mail nil)
-                 (nnfolder-inhibit-expiry t)))
-
-
-(setq imap-shell-program "MAIL=maildir:$HOME/Media/Document/Mail/ /usr/libexec/dovecot/imap")
-
+(setq gnus-startup-file "~/Media/Document/Emacs/Gnus/newsrc")
 (setq gnus-article-save-directory "~/Media/News/")
 (setq gnus-cache-directory "~/Media/News/Cache/")
 (setq nndraft-directory "~/Media/News/Drafts/")
