c073a87acdb2ee533163927283dba182ec82866a
Edward Bates <gitmux@stemux.com>
2022-08-24 17:25:49 -0500

    Configuration/Mode/Lambda-line/lambda-line.el: Inception
    Enable `lambda-line-mode' and define 'lambda-line-*' variables.
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ Configuration/Mode/Lambda-line/lambda-line.el
@@ -0,0 +1,9 @@
+(load-file "~/Internet/Git/Emacs/lambda-line/lambda-line.el")
+
+(setq lambda-line-position 'top)
+(setq lambda-line-space-top +0.30)
+(setq lambda-line-space-bottom -0.30)
+(setq lambda-line-symbol-position 0.1)
+(setq lambda-line-prefix t)
+
+(lambda-line-mode 1)
