8e685e57a750b9bb446b5cb23fa80882d65d1bf0
Edward Bates <gitmux@stemux.com>
2022-08-24 17:44:47 -0500

    Configuration/Mode/Enable/enable-mode.el: Remove cruft
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Enable/enable-mode.el
+++ Configuration/Mode/Enable/enable-mode.el
@@ -1,13 +1,7 @@
 (auto-save-visited-mode 1)
 (display-time-mode	1)
-(electric-pair-mode	1)
 (global-hl-line-mode	1)
 (save-place-mode	1)
 (savehist-mode		1)
 (show-paren-mode	1)
 
-(load-file "~/Internet/Git/Emacs/slow-keys/slow-keys.el")
-
-(slow-keys-mode)
-
-(add-to-list 'slow-keys-prohibited-command 'magit-status)
