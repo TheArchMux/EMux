e545b97622b5fd32bfcbd2ab9797f1c25ba88d04
Edward Bates <gitmux@stemux.com>
2022-08-24 17:38:24 -0500

    Configuration/Mode/load.el: Include relevant configuration file
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/load.el
+++ Configuration/Mode/load.el
@@ -4,6 +4,8 @@
 
 ;; (load-relative "God-Mode/god-mode.el")
 
+(load-relative "Golden-Ratio/golden-ratio.el")
+
 (load-relative "CC/cc.el")
 
 (load-relative "Ediff/ediff.el")
@@ -16,9 +18,17 @@
 
 ;; (load-relative "Hammy/hammy.el")
 
+(load-relative "Ido/ido.el")
+
+(load-relative "Lambda-line/lambda-line.el")
+
+(load-relative "Mailutils/mailutils.el")
+
 (load-relative "Org/org.el")
 
 ;;(load-relative "Polyp/polyp.el")
+
+(load-relative "Pulsar/pulsar.el")
 
 ;; (load-relative "Ryo/ryo.el")
 
