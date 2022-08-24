6c79b18591fb5d8313767bf5c3df95c12b038630
Edward Bates <gitmux@stemux.com>
2022-08-24 17:37:48 -0500

    Configuration/Visual/theme.el: Return to `mindre'
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Visual/theme.el
+++ Configuration/Visual/theme.el
@@ -2,5 +2,6 @@
 
 (add-to-list 'load-path "~/Internet/Git/Emacs/almost-mono-themes/")
 (add-to-list 'custom-theme-load-path "~/Internet/Git/Emacs/almost-mono-themes/")
+(load-file "~/Internet/Git/Emacs/mindre-theme/mindre-theme.elc")
 (load-file "~/.config/emacs/elpaca/builds/dash/dash.elc")
-(load-theme 'almost-mono-white t)
+(load-theme 'mindre t)
