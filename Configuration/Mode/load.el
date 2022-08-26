00ab8f8bb16772fbe5b7383d041263a6a02af880
Edward Bates <gitmux@stemux.com>
2022-08-25 22:02:43 -0500

    Configuration/Mode/load.el: Remove cruft
    Unnecessary files and related modes
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/load.el
+++ Configuration/Mode/load.el
@@ -2,10 +2,6 @@
 
 (load-relative "Enable/enable-mode.el")
 
-;; (load-relative "God-Mode/god-mode.el")
-
-(load-relative "Golden-Ratio/golden-ratio.el")
-
 (load-relative "CC/cc.el")
 
 (load-relative "Ediff/ediff.el")
@@ -16,22 +12,14 @@
 
 (load-relative "Goto-last-point/goto-last-point.el")
 
-;; (load-relative "Hammy/hammy.el")
-
-(load-relative "Ido/ido.el")
-
-(load-relative "Lambda-line/lambda-line.el")
-
 (load-relative "Mailutils/mailutils.el")
 
 (load-relative "Org/org.el")
 
-;;(load-relative "Polyp/polyp.el")
-
 (load-relative "Pulsar/pulsar.el")
 
-;; (load-relative "Ryo/ryo.el")
+(load-relative "Purpose/purpose.el")
 
 (load-relative "Tmr/tmr.el")
 
-(load-relative "Smartparens/smartparens.el")
+(load-relative "Zoom/zoom.el")
