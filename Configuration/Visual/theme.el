c779ae63189e6e2503c52aefe96a648ff3bdda5c
Edward Bates <gitmux@stemux.com>
2022-08-30 18:04:28 -0500

    Configuration/Visual/theme.el: Finalize theme
    Set theme to `almost-mono-white'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Visual/theme.el
+++ Configuration/Visual/theme.el
@@ -2,6 +2,5 @@
 
 (add-to-list 'load-path "~/Internet/Git/Emacs/almost-mono-themes/")
 (add-to-list 'custom-theme-load-path "~/Internet/Git/Emacs/almost-mono-themes/")
-(load-file "~/Internet/Git/Emacs/mindre-theme/mindre-theme.elc")
-(load-file "~/.config/emacs/elpaca/builds/dash/dash.elc")
-(load-theme 'mindre t)
+
+(load-theme 'almost-mono-white t)
