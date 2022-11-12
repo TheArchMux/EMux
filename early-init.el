2206321a22e5737d9c44e55de8a03df1215e4236
Edward Bates <gitmux@stemux.com>
2022-11-12 08:21:20 -0600

    early-init.el: Various updates
    * Load package `no-littering'
    	Requires `compat'
    
    * Increase Iosevka `height':
    	'105' -> '120'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- early-init.el
+++ early-init.el
@@ -1,3 +1,15 @@
+(add-to-list 'load-path "~/Internet/Git/Emacs/Utility/compat/")
+(require 'compat)
+
+(setq no-littering-etc-directory
+      (expand-file-name "No-Littering/Config/" user-emacs-directory))
+(setq no-littering-var-directory
+      (expand-file-name "No-Littering/Data/" user-emacs-directory))
+
+(load-file "~/Internet/Git/Emacs/Utility/no-littering.git/no-littering.el")
+
+(require 'no-littering)
+
 (setq wymux--theme 'lambda-light)
 
 (add-to-list 'load-path "~/Internet/Git/Emacs/Theme/lambda-themes.git/")
@@ -13,11 +25,11 @@
 (fringe-mode 0)
 (recentf-mode 1)
 
-(set-face-attribute 'default nil :family  "Iosevka" :height 105 :weight 'regular)
+(set-face-attribute 'default nil :family  "Iosevka" :height 120 :weight 'normal)
 (setq-default line-spacing 0.00)
 (setq initial-buffer-choice 'recentf-open-more-files)
 
 (setq wymux--light-theme 'lambda-light)
-(setq wymux--dark-theme 'lambda-dark)
+(setq wymux--dark-theme 'lambda-dark) 
 
 (global-font-lock-mode -1)
