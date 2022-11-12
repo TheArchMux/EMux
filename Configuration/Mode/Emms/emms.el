65cac5bd9c0ba736964cd81275f9d8699fc94574
Edward Bates <gitmux@stemux.com>
2022-11-12 08:29:42 -0600

    Configuration/Mode/Emms/emms.el: Various updates
    * Integrate with `mpd'
    
    * Integrate with `exiftool'
    
    * Set filter to `all-file'
    
    * Sync clear `emms' to `mpd'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Emms/emms.el
+++ Configuration/Mode/Emms/emms.el
@@ -1,12 +1,26 @@
-;; (add-to-list 'load-path "~/Internet/Git/Emacs/Music/emms/")
 (setq emms-source-file-default-directory "~/Media/Musica/")
+(setq emms-player-mpd-music-directory "~/Media/Musica/")
 
 (require 'emms-setup)
+(require 'emms-player-mpd)
+(require 'emms-mark)
+
 (emms-history-load)
-(emms-all)
-(emms-default-players)
+(emms-minimalistic)
 
+(setq emms-player-mpd-server-name "localhost")
+(setq emms-player-mpd-server-port "6600")
+(add-to-list 'emms-info-functions 'emms-info-mpd)
+(add-to-list 'emms-player-list 'emms-player-mpd)
 
+(setq emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
 
+(require 'emms-info-exiftool)
+(setq emms-info-functions '(emms-info-exiftool))
 
+(emms-browser-make-filter
+ "all-files" (emms-browser-filter-only-type 'file))
 
+(emms-player-mpd-connect)
+
+(add-hook 'emms-playlist-cleared-hook 'emms-player-mpd-clear)
