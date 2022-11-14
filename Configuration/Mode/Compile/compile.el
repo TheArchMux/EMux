e0ff616c28bf63f365ef0a2ce348dbe483296b24
Edward Bates <gitmux@stemux.com>
2022-11-13 22:40:11 -0600

    compile.el: Relocate `compilation-finish-functions' hook from minor.el
    * Directory: Configuration/Mode/Compile/ <- Configuration/Mode/Minor/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Compile/compile.el
+++ Configuration/Mode/Compile/compile.el
@@ -2,4 +2,14 @@
 (global-set-key (kbd "C-c m r")	'recompile)
 (global-set-key (kbd "C-c m c")	'compile)
 
+(add-hook 'compilation-finish-functions
+  (lambda (buf str)
+    (if (null (string-match ".*exited abnormally.*" str))
+        ;;no errors, make the compilation window go away in a few seconds
+        (progn
+          (run-at-time
+           "2 sec" nil 'delete-windows-on
+           (get-buffer-create "*compilation*"))
+          (message "No Compilation Errors!")))))
+
 
