da3e5284098e6ac22e7153073db0484ba6c97a5a
Edward Bates <gitmux@stemux.com>
2022-11-13 22:32:34 -0600

    compile.el: Delete `display-buffer-alist' definition
    * Directory: Configuration/Mode/Compile/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Compile/compile.el
+++ Configuration/Mode/Compile/compile.el
@@ -2,9 +2,4 @@
 (global-set-key (kbd "C-c m r")	'recompile)
 (global-set-key (kbd "C-c m c")	'compile)
 
-(add-to-list 'display-buffer-alist '("\*compilation\*" display-buffer-in-side-window
-				     (side . bottom)
-				     (slot . 1)
-				     (window-height . 0.35)))
-
 
