b3c1dbda2c3457ee1d20bde47123f3e8d0cffc2c
Edward Bates <gitmux@stemux.com>
2022-11-14 08:27:49 -0600

    exwm.el: Add `emms-player-mpd-disconnect' to `wymux/exwm-kill'
    * `wimux' -> 'wymux'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Function/Exwm/exwm.el
+++ Configuration/Function/Exwm/exwm.el
@@ -1,6 +1,7 @@
-(defun wimux/exwm-kill ()
+(defun wymux/exwm-kill ()
   "Kill exwm and emacs."
   (interactive)
   (recentf-save-list)
+  (emms-player-mpd-disconnect)
   (exwm-exit)
   (kill-emacs))
