91f190dca42b29b2230a9c2c56789ac90b9f6e2e
Edward Bates <gitmux@stemux.com>
2022-11-13 21:50:31 -0600

    early-init.el: Sort `set-face-attribute' lines
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- early-init.el
+++ early-init.el
@@ -13,13 +13,13 @@
 (setq wymux--theme 'brutalist)
 
 (set-face-attribute 'default nil :family  "Iosevka" :height 120 :weight 'normal)
-(set-face-attribute 'variable-pitch nil :family "Iosevka" :height 1.0)
+(set-face-attribute 'feebleline-dir-face nil :font "Iosevka" :height 120)
 (set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 1.0)
+(set-face-attribute 'minibuffer-prompt nil :font "Iosevka" :height 120)
 (set-face-attribute 'mode-line nil :font "Iosevka" :height 120)
 (set-face-attribute 'mode-line-buffer-id nil :font "Iosevka" :height 120)
 (set-face-attribute 'mode-line-emphasis nil :font "Iosevka" :height 120)
-(set-face-attribute 'minibuffer-prompt nil :font "Iosevka" :height 120)
-(set-face-attribute 'feebleline-dir-face nil :font "Iosevka" :height 120)
+(set-face-attribute 'variable-pitch nil :family "Iosevka" :height 1.0)
 
 (add-to-list 'load-path "~/Internet/Git/Emacs/Theme/brutalist-theme.el.git/")
 (add-to-list 'custom-theme-load-path "~/Internet/Git/Emacs/Theme/brutalist-theme.el.git/")
