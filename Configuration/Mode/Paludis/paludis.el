f4c7395fa895927f37b443e29f76c87e75892a89
Edward Bates <gitmux@stemux.com>
2022-11-18 14:00:48 -0600

    paludis.el: Define function `wymux/read--package-from-cache'
    * Directory[paludis.el]: Configuration/Mode/Paludis/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Paludis/paludis.el
+++ Configuration/Mode/Paludis/paludis.el
@@ -11,6 +11,13 @@
 	(find-args (concat "-name " exheres-file "*.ex*")))
     (find-dired exheres-directory find-args)))
 
+(defun wymux/read--package-from-cache ()
+  "Return one line from `paludis-cache-file'."
+  (let ((all-packages (with-temp-buffer
+			(insert-file-contents paludis-cache-file)
+			(split-string (buffer-string) "\n" t))))
+    (ido-completing-read "Package: " all-packages)))
+
 (setq exheres-directory "~/Internet/Git/Exherbo/")
 
 (setq paludis-cache-file "~/.cache/cave_all")
