4a574d3bbc8af02d4142746bfcbb793dbd7bcdfa
Edward Bates <gitmux@stemux.com>
2022-11-18 13:42:42 -0600

    paludis.el: Define variable `exheres-directory'
    * Directory[paludis.el]: Configuration/Mode/Paludis/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Paludis/paludis.el
+++ Configuration/Mode/Paludis/paludis.el
@@ -1,7 +1,11 @@
+(defvar exheres-directory nil
+  "Exheres local directory.")
+
 (defun wymux/find-exheres ()
   "Find exheres in exheres-directory."
   (interactive)
   (let* ((exheres-file (read-from-minibuffer "Exheres to locate: "))
-	(exheres-directory "~/Internet/Git/Exherbo")
 	(find-args (concat "-name " exheres-file "*.ex*")))
     (find-dired exheres-directory find-args)))
+
+(setq exheres-directory "~/Internet/Git/Exherbo/")
