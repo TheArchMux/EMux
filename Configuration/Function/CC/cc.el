98760f2d84568f124fe213f6908c1dbcd46ad00b
Edward Bates <gitmux@stemux.com>
2022-11-21 15:01:58 -0600

    cc.el: Define function `wymux/newline-parameter'
    * Directory[cc.el]: Configuration/Function/CC/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Function/CC/cc.el
+++ Configuration/Function/CC/cc.el
@@ -6,3 +6,19 @@
   (search-forward ";" nil t)
   (backward-delete-char 1)
   (insert " {\n\t\n}"))))
+
+(defun wymux/newline-parameter ()
+  "Set each parameter on a newline."
+  (interactive)
+  (search-backward "(")
+  (forward-char)
+  (set-mark-command 1)
+  (search-forward ")" nil t)
+  (backward-char)
+  (narrow-to-region (mark) (point))
+  (beginning-of-line)
+  (while
+      (search-forward " " nil t)
+    (newline))
+  (widen)
+  (c-indent-defun))
