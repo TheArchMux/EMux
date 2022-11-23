66fb17c3a9b63cb585bcf681997efd7d381128ab
Edward Bates <gitmux@stemux.com>
2022-11-23 08:48:10 -0600

    exwm.el: Define functions `wymux/term-{above,below}'
    * Directory[exwm.el]: Configuration/Function/Exwm/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Function/Exwm/exwm.el
+++ Configuration/Function/Exwm/exwm.el
@@ -5,3 +5,16 @@
   (emms-player-mpd-disconnect)
   (exwm-exit)
   (kill-emacs))
+
+(defun wymux/term-below ()
+  "Open `term' in window below."
+  (interactive)
+  (split-window-vertically)
+  (windmove-down)
+  (term "mksh"))
+
+(defun wymux/term-above ()
+  "Open `term' in window above."
+  (interactive)
+  (split-window-vertically)
+  (term "mksh"))
