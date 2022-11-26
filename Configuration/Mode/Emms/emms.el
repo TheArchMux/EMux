0dd764ac6ba3067dbe758c0769c9151492cda7d0
Edward Bates <gitmux@stemux.com>
2022-11-26 11:18:32 -0600

    emms.el: Redefine `emms-player-mpd-supported-regexp'
    * Directory[emms.el]: Configuration/Mode/Emms/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Emms/emms.el
+++ Configuration/Mode/Emms/emms.el
@@ -10,11 +10,20 @@
 (emms-history-load)
 (emms-minimalistic)
 
+(setq emms-player-mpd-supported-regexp (mapconcat 'regexp-quote '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma" ".dsf"
+								  ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
+								  ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a" ".flv" ".ogv" ".pls") "\\|"))
+
+(emms-player-set emms-player-mpd
+		 'regex
+		 emms-player-mpd-supported-regexp)
+
 (setq emms-player-mpd-server-name "localhost")
 (setq emms-player-mpd-server-port "6600")
 (add-to-list 'emms-info-functions 'emms-info-mpd)
 (add-to-list 'emms-player-list 'emms-player-mpd)
 
+(setq emms-source-file-gnu-find "/usr/bin/find")
 (setq emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
 
 (setq emms-info-functions '(emms-info-exiftool))
@@ -23,10 +32,5 @@
  "all-files" (emms-browser-filter-only-type 'file))
 
 (add-hook 'emms-playlist-cleared-hook 'emms-player-mpd-clear)
-
-(setq emms-player-mpd-supported-regexp
-      (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma" ".dsf"
-                    ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
-                    ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a" ".flv" ".ogv" ".pls")))
 
 (setq emms-history-start-playing t)
