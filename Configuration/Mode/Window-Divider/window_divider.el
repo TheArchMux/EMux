7e862841b9a436cb39c23c545de5ae4e72d1cd7c
Edward Bates <gitmux@stemux.com>
2022-12-25 11:25:42 -0600

    window_divider.el: Set `*-width' to '4'
    * Directory: Configuration/Mode/Window-Divider/
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/Window-Divider/window_divider.el
+++ Configuration/Mode/Window-Divider/window_divider.el
@@ -1,3 +1,4 @@
 (setq window-divider-default-places t)
-(setq window-divider-default-bottom-width 1)
-(setq window-divider-default-right-width 1)
+(setq window-divider-default-bottom-width 4)
+(setq window-divider-default-right-width 4)
+(window-divider-mode 1)
