10dcf440449d6eba15a3b10c93e0c983ff8865d0
Edward Bates <gitmux@stemux.com>
2023-01-24 20:05:54 -0600

    ggtags.el: Define "${GTAGSTHROUGH}" = "true"
    Directory: 'Configuration/Mode/GGtags/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Configuration/Mode/GGtags/ggtags.el
+++ Configuration/Mode/GGtags/ggtags.el
@@ -1,5 +1,7 @@
 (load-file "~/Internet/Git/Emacs/Mode/ggtags.git/ggtags.el")
 
+(setenv "GTAGSTHROUGH" "true")
+
 (add-hook 'c-mode-common-hook
 	  (lambda ()
 	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
