8953186a2af6f05c3b2fae98e222d83d18f72d73
Edward Bates <gitmux@stemux.com>
2023-02-27 18:26:46 -0600

    hook.el: Delete '*compilation*' in 1 second
    * Directory: 'config/etc/hook/hook/'

--- config/etc/hook/hook.el
+++ config/etc/hook/hook.el
@@ -18,7 +18,7 @@
 	    (if (null (string-match ".*exited abnormally.*" str))
 		(progn
 		  (run-at-time
-		   "2 sec" nil 'delete-windows-on
+		   "1 sec" nil 'delete-windows-on
 		   (get-buffer-create "*compilation*"))
 		  (message "No Compilation Errors!")))))
 
