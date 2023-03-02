a5546ed6b8d8451a2c5a2cfb6dc3a6546f644247
Edward Bates <gitmux@stemux.com>
2023-03-02 10:40:34 -0600

    completion.el: Rely on only `fussy'
    * Directory: 'config/etc/completion/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- config/etc/completion/completion.el
+++ config/etc/completion/completion.el
@@ -2,12 +2,9 @@
 (setq fussy-filter-fn 'fussy-filter-default)
 (fzf-native-load-dyn)
 
-(add-to-list 'completion-styles 'fussy t)
 (setq completion-category-overrides nil)
 (setq completion-category-defaults nil)
 
 (setq completion-styles '(fussy))
 
 (setq fussy-use-cache t)
-
-(setq completion-styles '(basic partial-completion fussy))
