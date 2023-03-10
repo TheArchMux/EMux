919697a73f7a914039a54d37a9d06320a6030b39
Edward Bates <gitmux@stemux.com>
2023-03-10 11:23:56 -0600

    display.el: Define '*Help*' -> `display-buffer-same-window'
    * Directory: 'root/etc/display/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/display/display.el
+++ root/etc/display/display.el
@@ -1,3 +1,6 @@
 (setq display-buffer-alist
-	     '(("\\*compilation*"
-		(display-buffer-no-window))))
+      '(
+	("\\*compilation*"
+		(display-buffer-no-window))
+	       ("*Help*"
+		(display-buffer-same-window))))
