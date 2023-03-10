fea447536e02dfb7aa142081af3f4c252faf792a
Edward Bates <gitmux@stemux.com>
2023-03-09 21:10:38 -0600

    load.el: Load additional files
    * Directory: 'root/etc/init/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/init/load.el
+++ root/etc/init/load.el
@@ -2,19 +2,26 @@
 (add-to-list 'load-path "~/Internet/Git/Emacs/Window-Manager/xelb/")
 
 (add-to-list 'load-path "~/Internet/Git/Emacs/Utility/vertico.git/")
-(add-to-list 'load-path "~/Internet/Git/Emacs/Utility/vertico.git/extensions")
 (add-to-list 'load-path "~/Internet/Git/Emacs/Utility/fzf-native.git/")
 (add-to-list 'load-path "~/Internet/Git/Emacs/Completion/flx.git")
 (load-file "~/Internet/Git/Emacs/Completion/fussy.git/fussy.el")
 (load-file "~/Internet/Git/Emacs/Utility/emacs-eat.git/eat.el")
+(load-file "~/Internet/Git/Emacs/Mode/emacs-coterm.git/coterm.el")
 
 (load-file "~/Internet/Git/Emacs/Modeline/feebleline/feebleline.el")
+(load-file "~/Internet/Git/Emacs/Utility/tempel.git/tempel.el")
+(load-file "~/Internet/Git/Emacs/Utility/speed-type.git/speed-type.el")
 
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/emms/")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/")
 (load-file "/usr/share/emacs/site-lisp/mailutils-mh.el")
+(load-file "/usr/share/gtags/gtags.el")
+(add-to-list 'load-path "~/Internet/Git/Emacs/Utility/paper-mode/")
+(add-to-list 'load-path "~/Internet/Git/Emacs/Utility/with-editor/lisp/")
+(add-to-list 'load-path "~/Internet/Git/Emacs/Utility/dash.el/")
+(add-to-list 'load-path "~/Internet/Git/Emacs/Git/magit/lisp/")
 
 (require 'eat)
+(require 'eglot)
 (require 'emms)
 (require 'emms-history)
 (require 'emms-playlist-mode)
@@ -24,5 +31,6 @@
 (require 'exwm-randr)
 (require 'fzf-native)
 (require 'vertico)
-(require 'vertico-grid)
 
+(require 'paper)
+(require 'magit)
