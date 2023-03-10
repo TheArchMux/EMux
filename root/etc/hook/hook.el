e773acb5bcee2325578a11908f5fbe107550ae36
Edward Bates <gitmux@stemux.com>
2023-03-09 21:09:12 -0600

    hook.el: `add-hook'{s}
    * Directory: 'root/etc/hook/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/hook/hook.el
+++ root/etc/hook/hook.el
@@ -1,3 +1,12 @@
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+(add-hook 'eshell-load-hook 'abbrev-mode)
 (add-hook 'eshell-load-hook 'eat-eshell-mode)
+(add-hook 'makefile-mode 'abbrev-mode)
+(add-hook 'prog-mode-hook 'abbrev-mode)
+(add-hook 'shell-mode-hook 'abbrev-mode)
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(add-hook 'rust-mode-hook 'eglot-ensure)
 
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 (add-to-list 'find-file-not-found-functions 'wymux/make-directory)
