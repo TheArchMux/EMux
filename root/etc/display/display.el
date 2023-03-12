9deb8e438869d60e960e64155ee648b27222625d
Edward Bates <gitmux@stemux.com>
2023-03-11 21:05:45 -0600

    display.el: Define "*Help*" and "magit: *"
    * Directory: 'root/etc/display/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/display/display.el
+++ root/etc/display/display.el
@@ -1,6 +1,8 @@
 (setq display-buffer-alist
       '(
 	("\\*compilation*"
-		(display-buffer-no-window))
-	       ("*Help*"
-		(display-buffer-same-window))))
+	 (display-buffer-no-window))
+	("*Help*"
+	 (display-buffer-same-window))
+	("magit: *"
+	 (display-buffer-same-window))))
