7d466d196232a7143d22bc532555912ee6225fcf
Edward Bates <gitmux@stemux.com>
2023-03-12 21:08:00 -0500

    search.el: Inception
    * Directory: 'root/bin/search'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ root/bin/search/search.el
@@ -0,0 +1,27 @@
+(defcustom wymux--search-term nil
+  "Current search term.")
+
+(defun wymux/search ()
+  "Search without editing interface.
+Created: Thursday, March-09-2023 18:41:26
+Revision: Thursday, March-09-2023 22:03:08"
+  (interactive)
+  (let ((search-term (read-from-minibuffer "Search: ")))
+    (setq wymux--search-term search-term)
+    (wymux/search-forward)))
+
+(defun wymux/search-forward ()
+  "Search next for 'wymux--search-term'.
+Created: Thursday, March-09-2023 18:46:45"
+  (interactive)
+  (unless (boundp 'wymux--search-term)
+    (wymux/search))
+  (search-forward wymux--search-term nil t))
+
+(defun wymux/search-backward ()
+  "Search backward for 'wymux--search-term'.
+Created: Thursday, March-09-2023 18:51:29"
+  (interactive)
+  (unless (boundp 'wymux--search-term)
+    (wymux/search))
+  (search-backward wymux--search-term nil t))
