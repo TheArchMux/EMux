2e3535a4aabbafb1179c2d22ddb382de11dfee5e
Edward Bates <gitmux@stemux.com>
2023-03-15 16:28:26 -0500

    dired.el: Define `wymux/dired-up'
    * Directory: 'root/bin/dired'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/dired/dired.el
+++ root/bin/dired/dired.el
@@ -8,3 +8,8 @@ Created: Tuesday, March-14-2023 08:58:09"
     (shell-command (concat binary file))
     (dired-revert)))
 
+(defun wymux/dired-up ()
+  "Dired in parent directory.
+Created: Tuesday, March-14-2023 12:05:17"
+  (interactive)
+  (dired "../."))
