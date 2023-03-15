a0ebd5ea7632dfab2cd8c5cb4eb76a82e3539457
Edward Bates <gitmux@stemux.com>
2023-03-15 16:29:21 -0500

    hook.el: Define additional hooks
    * Directory: 'root/etc/hook'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/hook/hook.el
+++ root/etc/hook/hook.el
@@ -1,3 +1,9 @@
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when  (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
+(add-hook 'c-mode-common-hook 'wymux/keymux-c-map)
+;; (add-hook 'change-major-mode-hook 'wymux/unbind-keys)
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 (add-hook 'eshell-load-hook 'abbrev-mode)
 (add-hook 'eshell-load-hook 'eat-eshell-mode)
