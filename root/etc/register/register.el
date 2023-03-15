18bc65e6bef65c939edfeb4482fe4e66ebc37887
Edward Bates <gitmux@stemux.com>
2023-03-15 16:22:25 -0500

    register.el: Define register 'd'
    * Directory: 'root/etc/register'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/register/register.el
+++ root/etc/register/register.el
@@ -1,3 +1,4 @@
 (set-register ?c '(file . "~/.config/emacs/root/"))
+(set-register ?d '(file . "~/Media/Document/"))
 (set-register ?e '(file . "~/Internet/Git/Exherbo"))
 (set-register ?g '(file . "~/Internet/Git"))
