386bc4bd1d9427f0b65ebc54d4091d60b61020e2
Edward Bates <gitmux@stemux.com>
2023-03-15 16:26:30 -0500

    point.el: Define `wymux/extract-url-at-point'
    * Directory: 'root/bin/point'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/point/point.el
+++ root/bin/point/point.el
@@ -1,6 +1,12 @@
 (defun wymux/extract-word-at-point ()
   "Extract word surrounding point."
-  (interactive)
   (let ((bounds (bounds-of-thing-at-point 'symbol)))
+    (when bounds
+      (message (buffer-substring-no-properties (car bounds) (cdr bounds))))))
+
+(defun wymux/extract-url-at-point ()
+  "Extract url at current point.
+Created: Wednesday, March-15-2023 07:57:10"
+  (let ((bounds (bounds-of-thing-at-point 'url)))
     (when bounds
       (message (buffer-substring-no-properties (car bounds) (cdr bounds))))))
