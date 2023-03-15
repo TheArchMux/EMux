59c4a901c36b9cdc7a0cedfb9d5e47d72b717108
Edward Bates <gitmux@stemux.com>
2023-03-15 16:29:41 -0500

    buffer.el: Define functions
    * Directory: 'root/bin/buffer'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/buffer/buffer.el
+++ root/bin/buffer/buffer.el
@@ -3,3 +3,33 @@
   (interactive)
   (kill-this-buffer)
   (delete-window))
+
+(defun wymux/quote-symbol ()
+  "Quote symbol at point.
+Created: Tuesday, March-14-2023 11:53:55"
+  (interactive)
+  (let ((symbol (wymux/extract-word-at-point)))
+    (next-line -1)
+    (replace-regexp symbol (concat "\"" symbol "\""))))
+
+(defun wymux/kill-whole-buffer ()
+  "Yank buffer into kill-ring.
+Created: Tuesday, March-14-2023 12:39:29"
+  (interactive)
+  (let ((p1 (point-min))
+	(p2 (point-max)))
+    (kill-ring-save p1 p2)))
+
+(defun wymux/kill-all-buffer ()
+  "Kill all other buffers.
+Created: Tuesday, March-14-2023 18:45:04"
+  (interactive)
+  (mapc 'kill-buffer (cdr (buffer-list))))
+
+(defun wymux/quote-url ()
+  "Quote url at point.
+Created: Wednesday, March-15-2023 07:58:40"
+  (interactive)
+  (let ((url (wymux/extract-url-at-point)))
+    (next-line -1)
+    (replace-regexp (regexp-quote url) (concat "\"" url "\""))))
