50505635bb79b883a3a8dc8f109e41d6e026c8b8
Edward Bates <gitmux@stemux.com>
2023-03-21 17:15:46 -0500

    buffer.el: Define function
    * Directory: 'root/bin/buffer'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/buffer/buffer.el
+++ root/bin/buffer/buffer.el
@@ -1,3 +1,5 @@
+(setq wymux-scratch-buffer '("c" "cpp" "rst" "py" "go" "English" "Temporary"))
+
 (defun wymux/kill-buffer-delete-window ()
   "Combine `kill-buffer' and `delete-window'."
   (interactive)
@@ -11,6 +13,14 @@ Created: Tuesday, March-14-2023 11:53:55"
   (let ((symbol (wymux/extract-word-at-point)))
     (next-line -1)
     (replace-regexp symbol (concat "\"" symbol "\""))))
+
+(defun wymux/single-quote-symbol ()
+  "Quote symbol with single quote.
+Created: Thursday, March-16-2023 10:09:32"
+  (interactive)
+  (let ((symbol (wymux/extract-word-at-point)))
+    (next-line -1)
+    (replace-regexp symbol (concat "\'" symbol "\'"))))
 
 (defun wymux/kill-whole-buffer ()
   "Yank buffer into kill-ring.
@@ -33,3 +43,10 @@ Created: Wednesday, March-15-2023 07:58:40"
   (let ((url (wymux/extract-url-at-point)))
     (next-line -1)
     (replace-regexp (regexp-quote url) (concat "\"" url "\""))))
+
+(defun wymux/switch-to-scratch ()
+  "Switch to custom scratch buffer.
+Created: Thursday, March-16-2023 10:38:51"
+  (interactive)
+  (let ((sractch (completing-read "Scratch: " wymux-scratch-buffer)))
+    (switch-to-buffer (concat "sratch." sractch))))
