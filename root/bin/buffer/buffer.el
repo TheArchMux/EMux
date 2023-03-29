bf19359ae7d50e4ab3c3b5330ff9dbea37e0e8d3
Edward Bates <gitmux@stemux.com>
2023-03-28 19:14:45 -0500

    buffer.el: Define additional function
    + `wymux/unquote-symbol'
    + `wymux/toggle-quote'
    + `wymux/kill-buffer-name'
    + `wymux/clean-buffer-list'
    + `wymux/goto-quote'
    + `wymux/edit-string'
    + `wymux/backward-whitespace'
    + `wymux/kill-word'
    + `wymux/edit-parenthesis'
    
    * Directory: 'root/bin/buffer'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/buffer/buffer.el
+++ root/bin/buffer/buffer.el
@@ -8,19 +8,30 @@
 
 (defun wymux/quote-symbol ()
   "Quote symbol at point.
-Created: Tuesday, March-14-2023 11:53:55"
+Created: Tuesday, March-14-2023 11:53:55
+Revised: Thursday, March-23-2023 18:27:32"
   (interactive)
-  (let ((symbol (wymux/extract-word-at-point)))
-    (next-line -1)
-    (replace-regexp symbol (concat "\"" symbol "\""))))
+  (save-excursion
+    (let ((symbol-bounds (bounds-of-thing-at-point 'symbol)))
+      (when symbol-bounds
+	(goto-char (car symbol-bounds))
+	(insert "\"")
+	(goto-char (+ 1 (cdr symbol-bounds)))
+	(insert "\"")))))
 
 (defun wymux/single-quote-symbol ()
-  "Quote symbol with single quote.
-Created: Thursday, March-16-2023 10:09:32"
-  (interactive)
-  (let ((symbol (wymux/extract-word-at-point)))
-    (next-line -1)
-    (replace-regexp symbol (concat "\'" symbol "\'"))))
+  "Quote symbol with single quote from double quote.
+Created: Thursday, March-16-2023 10:09:32
+Revised: Thursday, March-23-2023 18:22:57"
+  (save-excursion
+    (let ((symbol-bounds (bounds-of-thing-at-point 'symbol)))
+      (when symbol-bounds
+	(goto-char (car symbol-bounds))
+	(delete-backward-char 1)
+	(insert "'")
+	(goto-char (+ 1 (cdr symbol-bounds)))
+	(delete-backward-char 1)
+	(insert "'")))))
 
 (defun wymux/kill-whole-buffer ()
   "Yank buffer into kill-ring.
@@ -36,6 +47,17 @@ Created: Tuesday, March-14-2023 18:45:04"
   (interactive)
   (mapc 'kill-buffer (cdr (buffer-list))))
 
+(defun wymux/unquote-symbol ()
+  "Unquote symbol.
+Created: Thursday, March-23-2023 18:19:16"
+  (save-excursion
+    (let ((symbol-bounds (bounds-of-thing-at-point 'symbol)))
+      (when symbol-bounds
+	(goto-char (car symbol-bounds))
+	(delete-backward-char 1)
+	(goto-char (cdr symbol-bounds))
+	(delete-backward-char 1)))))
+
 (defun wymux/quote-url ()
   "Quote url at point.
 Created: Wednesday, March-15-2023 07:58:40"
@@ -50,3 +72,92 @@ Created: Thursday, March-16-2023 10:38:51"
   (interactive)
   (let ((sractch (completing-read "Scratch: " wymux-scratch-buffer)))
     (switch-to-buffer (concat "sratch." sractch))))
+
+(defun wymux/toggle-quote ()
+  "Toggle between single, double, and no quote.
+Created: Thursday, March-23-2023 18:13:23"
+  (interactive)
+  (unless (eq last-command this-command)
+    (put this-command 'state 0))
+  (cond
+   ((equal 0 (get this-command 'state))
+    (wymux/quote-symbol)
+    (put this-command 'state 1))
+   ((equal 1 (get this-command 'state))
+    (wymux/single-quote-symbol)
+    (put this-command 'state 2))
+   ((equal 2 (get this-command 'state))
+    (wymux/unquote-symbol)
+    (put this-command 'state 0))))
+   
+(defun wymux/kill-buffer-name (name)
+  "Kill buffer matching name.
+Created: Friday, March-24-2023 13:37:36"
+  (interactive "sBuffer name: ")
+  (dolist (buffer (buffer-list))
+    (when (string-prefix-p name (buffer-name buffer))
+      (kill-buffer buffer))))
+
+(setq wymux-clean-buffer-list '("*ediff-*" "*Ediff" "*Completions*" "*Buffer List*" "+Inbox"
+				"*Backtrace*" "*Warnings*" "*Messages*" "*GNU Emacs*"
+				"magit: *" "magit-*" "*Find*" "*Help*" "*scratch" "*Man*"
+				"*eshell" "*Async Shell Command*" "*Shell Command Output*"
+				"*Occur*" "*Apropos*"))
+
+(defun wymux/clean-buffer-list ()
+  "Clean buffer list
+Created: Friday, March-24-2023 14:36:34"
+  (interactive)
+  (let ((buffer-list 'wymux-clean-buffer-list))
+    (dotimes (i (length wymux-clean-buffer-list))
+      (ignore-errors
+	(kill-matching-buffers (nth i wymux-clean-buffer-list) nil t)))))
+
+(defun wymux/goto-quote ()
+  "Goto \".
+Created: Saturday, March-25-2023 21:37:26"
+  (interactive)
+  (search-forward "\""))
+
+(defun wymux/edit-string ()
+  "Edit next string.
+Created: Saturday, March-25-2023 21:38:23"
+  (interactive)
+  (wymux/goto-quote)
+  (zap-up-to-char 1 (string-to-char "\"")))
+
+(defun wymux/kill-paragraph ()
+  "Kill entire paragraph.
+Created: Saturday, March-25-2023 22:24:18"
+  (interactive)
+  (kill-paragraph -1)
+  (kill-paragraph 1))
+
+(defun wymux/backward-whitespace ()
+  "Backward whitespace.
+Created: Sunday, March-26-2023 07:36:31"
+  (interactive)
+  (forward-whitespace -1))
+
+(defun wymux/kill-word ()
+  "Kill entiree word.
+Created: Monday, March-27-2023 14:41:24"
+  (interactive)
+  (kill-word 1)
+  (backward-kill-word 1)
+  (keymux-modal-mode -1))
+
+(defun wymux/edit-parenthesis ()
+  "Edit contents inside of () pair.
+Created: Tuesday, March-28-2023 12:47:01"
+  (interactive)
+  (let ((p1)
+	(p2))
+    (search-backward "(")
+    (forward-char 1)
+    (setq p1 (point))
+    (up-list 1)
+    (backward-char 1)
+    (setq p2 (point))
+    (delete-region p1 p2)
+    (keymux-modal-mode -1)))
