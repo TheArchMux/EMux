6f03bc07506d2148078e1d00b1f4e45c58412797
Edward Bates <gitmux@stemux.com>
2023-03-30 18:13:48 -0500

    emms.el: Defune `wymux/emms-play-url'
    * Directory: 'root/bin/emms'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/emms/emms.el
+++ root/bin/emms/emms.el
@@ -4,3 +4,11 @@ Created: Friday, March-10-2023 11:19:33"
   (interactive)
   (let ((regexp (completing-read "Song: " nil)))
     (emms-play-find emms-source-file-default-directory regexp)))
+
+(defun wymux/emms-play-url ()
+  "`emms-play-url' from clipboard.
+Created: Wednesday, March-29-2023 20:54:47"
+  (interactive)
+  (let ((url (shell-command-to-string "xclip -o -selection clipboard")))
+    (message "Playing %s" url)
+    (emms-play-url url)))
