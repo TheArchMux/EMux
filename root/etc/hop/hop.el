21349fe7bacb31b4b28408e0051f804d35bc55ea
Edward Bates <gitmux@stemux.com>
2023-04-06 12:23:57 -0500

    hop.el: Remove comments
    * Directory: 'root/etc/hop'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/hop/hop.el
+++ root/etc/hop/hop.el
@@ -1,6 +1,1 @@
 (setq hop-hints-position 'start)
-
-;; (set-face-foreground 'hop-face-dim-unmatched "#8B4513")
-;; (set-face-foreground 'hop-face-single-char "#000000")
-;; (set-face-foreground 'hop-face-double-char-1 "#000000")
-;; (set-face-foreground 'hop-face-double-char-2 "#000000")
