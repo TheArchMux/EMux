4a6a265ee782fccc1b6d478b32a365e32260b6ef
Edward Bates <gitmux@stemux.com>
2023-04-06 12:02:49 -0500

    ggtags.el: Termination
    * Directroy: 'root/etc/ggtags'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/ggtags/ggtags.el
+++
@@ -1,6 +0,0 @@
-(define-key ggtags-navigation-map (kbd "C-j") 'previous-error)
-(define-key ggtags-navigation-map (kbd "C-\;") 'next-error)
-(define-key ggtags-navigation-map (kbd "C-k") 'ggtags-navigation-next-file)
-(define-key ggtags-navigation-map (kbd "C-l") 'ggtags-navigation-previous-file)
-(define-key ggtags-navigation-map (kbd "C-i") 'ggtags-navigation-start-file)
-(define-key ggtags-navigation-map (kbd "C-s") 'ggtags-navigation-isearch-forward)
