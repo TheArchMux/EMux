5981d73a889a7b8d3e8eaac0686188bae5a37871
Edward Bates <gitmux@stemux.com>
2023-04-06 11:46:40 -0500

    display.el: Add '*Completions*'
    * Directory: 'root/etc/display'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/display/display.el
+++ root/etc/display/display.el
@@ -2,7 +2,7 @@
       '(
 	("\\*compilation*\\|*Async*\\|*Warnings*"
 	 (display-buffer-no-window))
-	("*Help*\\|*Buffer List*\\|*Locate*\\|*Apropos*"
+	("*Help*\\|*Buffer List*\\|*Locate*\\|*Apropos*\\|*Completions*"
 	 (display-buffer-same-window))
 	("magit: *"
 	 (display-buffer-same-window))
