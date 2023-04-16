279fc40362b389d25ccf821b75e113c0c990bf70
Edward Bates <gitmux@stemux.com>
2023-04-16 09:11:38 -0500

    ibuffer.el: Additional cetegory
    ~ 'root/etc/ibuffer'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/etc/ibuffer/ibuffer.el
+++ root/etc/ibuffer/ibuffer.el
@@ -2,11 +2,26 @@
 
 (setq ibuffer-saved-filter-groups
       '(("default"
-		("College" (filename . "College"))
-		("Exherbo" (or
-			    (mode . exheres-mode)
-			    (mode . exlib-mode)
-			    (filename . "Exherbo"))))))
+	 ("College" (filename . "College"))
+	 ("Communication" (or
+			   (mode . mh-folder-mode)
+			   (name . "\\*MH-E Log\\*")))
+	 ("Document" (name . "Zathura*"))
+	 ("Emacs" (or
+		   (mode . backtrace-mode)
+		   (mode . Custom-mode)
+		   (mode . customize-mode)
+		   (mode . help-mode)
+		   (mode . completion-list-mode)
+		   (mode . messages-buffer-mode)
+		   (mode . occur-mode)
+		   (name . "^\\*scratch\\*$")
+		   (name . "^\\*GNU Emacs\\*$")))
+	 ("Exherbo" (or
+		     (mode . exheres-mode)
+		     (mode . exlib-mode)
+		     (filename . "Exherbo")))
+	 ("Emux" (filename . ".config/emacs/")))))
 
 (add-hook 'ibuffer-mode-hook
 	  (lambda ()
