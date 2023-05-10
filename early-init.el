879beb5110a33e1c834ccb1ad59c689098c117e8
Edward Bates <gitmux@stemux.com>
2023-05-09 20:48:20 -0500

    early-init.el: Load theme
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- early-init.el
+++ early-init.el
@@ -9,6 +9,9 @@
 (add-to-list 'load-path "~/Internet/Git/Emacs/Theme/tao-theme-emacs.git/")
 (add-to-list 'custom-theme-load-path "~/Internet/Git/Emacs/Theme/tao-theme-emacs.git/")
 
+(add-to-list 'load-path "~/Internet/Git/Emacs/Theme/lambda-themes.git/")
+(add-to-list 'custom-theme-load-path "~/Internet/Git/Emacs/Theme/lambda-themes.git/")
+
 (global-font-lock-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
