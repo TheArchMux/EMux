7d0620bb9c4faf94c4ac3085f641afd44c1dc0b9
Edward Bates <gitmux@stemux.com>
2023-05-18 18:26:19 -0500

    window.el: Define additional function
    - 'root/bin/window/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/window/window.el
+++ root/bin/window/window.el
@@ -1,13 +1,21 @@
-(defun wymux/split-move-horizontally ()
+(defun wymux/split-window-horizontally ()
   "Split horizontally then relocate point.
 Created: Friday, May-12-2023 17:49:41"
   (interactive)
   (split-window-horizontally)
   (windmove-down))
 
-(defun wymux/split-move-vertically ()
+(defun wymux/split-window-vertically ()
   "Split vertically then relocate point.
 Created: Friday, May-12-2023 17:50:41"
   (interactive)
   (split-window-vertically)
   (windmove-right))
+
+(defun wymux/update-window-modeline ()
+  "Update the modeline color based on whether the current window is active."
+  (interactive)
+  (if (eq (selected-window) (frame-selected-window))
+      (progn
+	(set-face-background 'mode-line "black")
+	(set-face-foreground 'mode-line "white"))))
