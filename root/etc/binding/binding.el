8bd634f74adf4eea5fb1d7b63464dd390a499d2a
Edward Bates <gitmux@stemux.com>
2023-05-24 21:47:34 -0500

    binding.el: Inception: Attempt non-modal
    - 'root/etc/binding/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ root/etc/binding/binding.el
@@ -0,0 +1,35 @@
+;; Editing
+
+(global-set-key (kbd "C-c d w") 'kill-whole-line)
+(global-set-key (kbd "C-c d d") 'duplicate-dwim)
+
+;; Filter
+
+(global-set-key (kbd "C-c s o") 'occur)
+
+;; Find
+
+(global-set-key (kbd "C-c f p") 'ffap)
+(global-set-key (kbd "C-c f r") 'wymux/recentf-find)
+(global-set-key (kbd "C-c f t") 'wymux/find-file-subdirectory)
+
+;; Non-editing programs
+
+(global-set-key (kbd "C-c i") 'ibuffer)
+(global-set-key (kbd "C-c p c") 'compile)
+(global-set-key (kbd "C-c p e") 'emms)
+(global-set-key (kbd "C-c p g") 'gnus)
+(global-set-key (kbd "C-c p m") 'mh-rmail)
+(global-set-key (kbd "C-c p t") 'emms-play-directory-tree)
+(global-set-key (kbd "C-c p w") 'wymux/firefox)
+
+;; Navigation
+
+(global-set-key (kbd "C--") 'wymux/backward-whitespace)
+(global-set-key (kbd "C-=") 'forward-whitespace)
+
+(global-set-key (kbd "C-c g c") 'hop-char)
+(global-set-key (kbd "C-c g l") 'hop-line)
+(global-set-key (kbd "C-c g s") 'hop-line-skip-whitespace)
+(global-set-key (kbd "C-c g w") 'hop-word)
+
