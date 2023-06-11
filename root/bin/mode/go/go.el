d73cd90b79b1c567b920413adacbae9024e7ec2c
Edward Bates <gitmux@stemux.com>
2023-06-11 10:20:14 -0500

    go.el: Define `wymux/go-format'
    - 'root/bin/mode/go/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/mode/go/go.el
+++ root/bin/mode/go/go.el
@@ -1,3 +1,12 @@
-(define-derived-mode go-mode prog-mode "Go mode"
-  "Go major mode."
-  )
+(defun wymux/go-format ()
+  "'gofmt' on buffer.
+Created: Sunday, April-23-2023 08:17:15"
+  (interactive)
+  (let ((name (buffer-file-name)))
+    (when name
+      (when (buffer-modified-p) (save-buffer))
+      (shell-command (format "gofmt -w %s" name))
+      (revert-buffer t t t))))
+
+(define-derived-mode wymux/go-mode prog-mode "Go"
+  "Mux's Go mode.")
