8b95647bd9eae2e5a7670d1bdedd867f5058893c
Edward Bates <gitmux@stemux.com>
2023-06-11 10:21:00 -0500

    scrach.el: Inception
    - 'root/{bin,etc}/scratch/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ root/bin/scratch/scratch.el
@@ -0,0 +1,6 @@
+(defun wymux/open-cpp-scratch ()
+  "Open cpp scratch buffer.
+Created: Saturday, April-22-2023 20:41:00"
+  (interactive)
+  (find-file-existing "~/Media/Document/Programming/Scratch/CPP/scratch.cpp"))
+  
---
+++ root/etc/scratch/scratch.el
@@ -0,0 +1,4 @@
+(defvar wymux-scratch-list '(
+			     ("c" .  "")))
+
+(customize-set-variable 'initial-scratch-message nil)
