7a99d727ce11e52f1c913a42ec767182ec4fd14e
Edward Bates <gitmux@stemux.com>
2023-06-13 17:59:50 -0500

    howm.el: Inception
    - 'root/etc/howm/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ root/etc/howm/howm.el
@@ -0,0 +1,3 @@
+(customize-set-variable 'howm-directory "~/Media/Document/Notes/howm/")
+(customize-set-variable 'howm-history-file "~/.cache/.howm-history")
+(customize-set-variable 'howm-keyword-file "~/.cache/.howm-keys")
