c7f5d4e12e4e3b667c769825f3cbc3563a0f4f59
Edward Bates <gitmux@stemux.com>
2023-06-14 14:05:40 -0500

    web.el: Inception
    - 'root/bin/web/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ root/bin/web/web.el
@@ -0,0 +1,14 @@
+(defun wymux/phind-text () 
+  "Query 'phind.com' with line's text.
+Created: Tuesday, June-13-2023 18:04:45"
+  (interactive)
+  (let ((text (replace-regexp-in-string "[[:space:]]" "%20" (thing-at-point 'line))))
+    (browse-url-firefox (concat "https://phind.com/search?q=" text "&source=searchbox"))))
+
+(defun wymux/github-text ()
+  "Query 'github.com' with line's text.
+Created: Wednesday, June-14-2023 13:30:46"
+  (interactive)
+  (let ((text (replace-regexp-in-string "[[:space:]]" "+" (thing-at-point 'line))))
+    (browse-url-firefox (concat "https://github.com/search?q=" text "&type=repositories&s=updated&o=desc"))))
+
