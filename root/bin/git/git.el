97da85cfa55b9d7a63d614adcabf712fbdbd771d
Edward Bates <gitmux@stemux.com>
2023-06-16 11:53:04 -0500

    git.el: Define functions
    * `wymux/git-modified-files'
    
    * `wymux/git-stage-this-file'
    
    - 'root/bin/git/'
    
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- root/bin/git/git.el
+++ root/bin/git/git.el
@@ -10,3 +10,26 @@ Created: Tuesday, June-13-2023 18:51:33"
   (interactive)
   (insert (file-name-directory (wymux/git-previous-commit-files))))
 
+(defun wymux/git-modified-files ()
+  "List modified files of git repository.
+Created: Friday, June-16-2023 11:09:12"
+  (interactive)
+  (let ((files))
+  (with-temp-buffer
+    (cd (file-name-parent-directory (string-trim (shell-command-to-string "git rev-parse --git-dir"))))
+    (setq files (string-trim (shell-command-to-string "git ls-files -m"))))
+  (get-buffer-create "Wymux git modified files")
+  (switch-to-buffer "Wymux git modified files")
+  (insert files)
+  (local-set-key "c" 'wymux/git-stage-this-file)
+  (local-set-key "q" 'kill-this-buffer)))
+
+(defun wymux/git-stage-this-file ()
+  "Commit line's file.
+Created: Friday, June-16-2023 11:43:12"
+  (interactive)
+  (let ((file (string-trim (thing-at-point 'line))))
+    (shell-command (concat "git add " file))
+    (mesage (concat ("Staged " file)))))
+
+
